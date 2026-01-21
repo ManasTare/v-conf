866a7e40018face84c2b7728f4907a573557c068
ManasTare <111227859+ManasTare@users.noreply.github.com>
2026-01-21 08:21:01 +0530

    Add files via upload

---
+++ v-conf/src/main/java/com/example/controller/DefaultConfigController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.dto.DefaultConfigResponseDTO;
+import com.example.service.DefaultConfigService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/default-config")
+@CrossOrigin
+public class DefaultConfigController {
+
+    private final DefaultConfigService service;
+
+    public DefaultConfigController(DefaultConfigService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{modelId}")
+    public DefaultConfigResponseDTO getDefaultConfig(
+            @PathVariable Integer modelId,
+            @RequestParam Integer qty
+    ) {
+        return service.getDefaultConfiguration(modelId, qty);
+    }
+}
