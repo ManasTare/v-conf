9fc79d34cef88fd5e1fb34310650a07d07c48e73
AditiJ125 <aditipjoglekar@gmail.com>
2026-01-23 13:49:49 +0530

    Test

--- v-conf/src/main/java/com/example/controller/UserController.java
+++ v-conf/src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.example.service.UserManager;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
 	@Autowired
